ee34ded02eb53b0ded852bc3ee16c1c7c151cd57
wjallen <wallen@tacc.utexas.edu>
2024-05-18 15:53:56 -0500

    rearrange test data

--- test/data/convert_xml_to_json.py
+++ test/data/convert_xml_to_json.py
@@ -4,11 +4,8 @@ import sys
 
 filename = sys.argv[1]
 
-
 with open(filename, 'r') as f:
     data = xmltodict.parse(f.read())
-
-print(data)
 
 with open(f'{filename.rsplit(".",1)[0]}.json', 'w') as o:
     json.dump(data, o, indent=2)
