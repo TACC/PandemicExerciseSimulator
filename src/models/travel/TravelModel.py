1a639b5d9436213808c976f559e57554fb5cd87a
wjallen <wallen@tacc.utexas.edu>
2024-06-02 09:43:21 -0500

    logging statement in base travel model

--- src/models/travel/TravelModel.py
+++ src/models/travel/TravelModel.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class TravelModel:
 
     def __init__(self):
+        logger.info(f'instantiated a TravelModel object: {TravelModel}')
         return
 
     def __str__(self) -> str:
