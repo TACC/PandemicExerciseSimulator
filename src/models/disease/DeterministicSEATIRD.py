b08a343a0170bb4f46da171515b0e1d549f3fe27
wjallen <wallen@tacc.utexas.edu>
2024-08-30 11:10:54 -0500

    template for deterministic seatird

--- src/models/disease/DeterministicSEATIRD.py
+++ src/models/disease/DeterministicSEATIRD.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python3
 import numpy as np
 from scipy.integrate import solve_ivp
-import matplotlib.pyplot as plt
 
-#S = Susceptible
-#E = Exposed
-#A = Asymptomatic infectious
-#T = Treated
-#I = Infectious symptomatic
-#R = Recovered
-#D = Deceased
+import logging
+from typing import Type
+
+from .DiseaseModel import DiseaseModel
+from baseclasses.Group import Group, RiskGroup, VaccineGroup
+from baseclasses.Network import Network
+from baseclasses.Node import Node
+
+logger = logging.getLogger(__name__)
+
 
 def SEATIRD_model(t, y, beta, nu, mu, tau, gamma, delta, rho):
     S, E, A, T, I, R, D = y
@@ -21,51 +24,108 @@ def SEATIRD_model(t, y, beta, nu, mu, tau, gamma, delta, rho):
     dR_dt = gamma * I + rho * T
     dD_dt = delta * I
     
+    logging.info(f'{dS_dt}, {dE_dt}, {dA_dt}, {dT_dt}, {dI_dt}, {dR_dt}, {dD_dt}')
     return [dS_dt, dE_dt, dA_dt, dT_dt, dI_dt, dR_dt, dD_dt]
 
-# Initial conditions
-S0 = 0.99
-E0 = 0.01
-A0 = 0
-T0 = 0
-I0 = 0
-R0 = 0
-D0 = 0
-initial_conditions = [S0, E0, A0, T0, I0, R0, D0]
 
-# Parameters
-beta = 0.3
-nu = 0.2
-mu = 0.1
-tau = 0.05
-gamma = 0.1
-delta = 0.01
-rho = 0.1
-params = (beta, nu, mu, tau, gamma, delta, rho)
+class DeterministicSEATIRD(DiseaseModel):
 
-# Time points
-t_span = (0, 160)
-t_eval = np.linspace(*t_span, 1000)
+    def __init__(self, disease_model:Type[DiseaseModel]):
+        self.is_stochastic = disease_model.is_stochastic
+        self.now = disease_model.now
+        self.parameters = disease_model.parameters
 
-# Solve ODE
-solution = solve_ivp(SEATIRD_model, t_span, initial_conditions, args=params, t_eval=t_eval, vectorized=True)
+        logger.info(f'instantiated DeterministicSEATIRD object with stochastic={self.is_stochastic}')
+        logger.debug(f'{self.parameters}')
+        return
 
-# Extract solutions
-S, E, A, T, I, R, D = solution.y
 
-# Plot results
-plt.figure(figsize=(10, 6))
-plt.plot(t_eval, S, label='Susceptible')
-plt.plot(t_eval, E, label='Exposed')
-plt.plot(t_eval, A, label='Asymptomatic Infectious')
-plt.plot(t_eval, T, label='Treated')
-plt.plot(t_eval, I, label='Infected (Symptomatic)')
-plt.plot(t_eval, R, label='Recovered')
-plt.plot(t_eval, D, label='Deceased')
+    def set_initial_conditions(self, initial:list, network:Type[Network]):
+        """
+        This method is invoked from the main simulator block. Read in the list of initial infected
+        per location per age group, and expose 
 
-plt.xlabel('Time')
-plt.ylabel('Proportion of Population')
-plt.title('SEATIRD Model')
-plt.legend()
-plt.grid()
-plt.show()
+        Args:
+            initial (list): list of initial infected per age group per county from INPUT
+            network (Network): network object with list of nodes
+        """
+        for item in initial:
+            # TODO the word "county" is hardcoded here but should be made dynamic in case
+            # people want to do zip codes instead. Maybe 'location_id'
+            this_node_id = int(item['county'])
+            this_infected = int(item['infected'])
+            this_age_group = int(item['age_group'])
+
+            group = Group(this_age_group, RiskGroup.L.value, VaccineGroup.U.value)
+
+            for node in network.nodes:
+                if node.node_id == this_node_id:
+                    self.expose_number_of_people(node, group, this_infected)
+
+        # TODO If this method remains the same between deterministic and stochastic,
+        # then I should move it into the parent class.
+        return
+
+
+    def expose_number_of_people(self, node:Type[Node], group:Type[Group], num_to_expose:int):
+        """
+        
+        """
+        node.compartments.expose_number_of_people(group, num_to_expose)
+        return
+    
+
+    def simulate(self, node, time):
+        """
+        Main simulation logic for deterministic SEATIRD model
+
+        E = Exposed, S = Susceptible, A = Asymptomatic infectious,
+        T = Treated, I = Infectious symptomatic, R = Recovered, D = Deceased
+        """
+        logger.debug(f'node={node}, time={time}')
+
+        initial_conditions = [
+            node.compartments.susceptible_population(),
+            node.compartments.exposed_population(),
+            node.compartments.asymptomatic_population(),
+            node.compartments.treatable_population(),
+            node.compartments.infectious_population(),
+            node.compartments.recovered_population(),
+            node.compartments.deceased_population(),
+        ]
+
+        model_parameters = (
+            0.3,
+            0.2,
+            0.1,
+            0.05,
+            0.1,
+            0.01,
+            0.1,
+        )
+
+        #model_parameters = (
+        #    self.parameters.beta,
+        #    self.parameters.nu,
+        #    self.parameters.mu,
+        #    self.parameters.tau,
+        #    self.parameters.gamma,
+        #    self.parameters.delta,
+        #    self.parameters.rho,
+        #)
+
+        t_span = (0, 1)
+        t_eval = np.linspace(*t_span, 1)
+
+        # Solve ODE
+        solution = solve_ivp(SEATIRD_model, t_span, initial_conditions, args=model_parameters, t_eval=t_eval, vectorized=True)
+
+        # Extract solutions
+        #S, E, A, T, I, R, D = solution.y
+        #logger.info(f'solution.y = {solution.y}')
+        return
+
+
+
+
+
