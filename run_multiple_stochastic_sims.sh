4273c10ed5245cac8be52ecfd87e12f6c6e042ab
Emily Javan <emjavan@gmail.com>
2025-06-12 09:51:44 -0500

    run deterministic as well

--- run_multiple_stochastic_sims.sh
+++ run_multiple_stochastic_sims.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-for i in {0..7}
+# Run deterministic model as well
+# This assumes all params are what you want already, e.g. mobility turned off etc
+mkdir -p OUTPUT_small_deterministic/
+poetry run python3 src/simulator.py -l INFO -d 30 -i data/texas/INPUT_small_deterministic.json
+echo "✨🌸🌙 Finished deterministic model"
+
+for i in {0..10}
 do
    # Define the full sim subdirectory path
-   sim_dir="OUTPUT_small_stochastic/OUTPUT_small_stochastic_sim${i}"
+   init_sim_dir="OUTPUT_small_stochastic_min1/OUTPUT_small_stochastic_min1_sim0"
+   sim_dir="OUTPUT_small_stochastic_min1/OUTPUT_small_stochastic_min1_sim${i}"
 
    # Create the sim directory if it doesn't exist
    mkdir -p "${sim_dir}"
@@ -12,16 +19,23 @@ do
    cp data/texas/INPUT_small.json tmp_input.json
 
    # Replace only the sim subfolder name using the sim_dir variable
-   sed -i '' "s|OUTPUT_small_stochastic/OUTPUT_small_stochastic_sim0|${sim_dir}|" tmp_input.json
+   sed -i '' "s|${init_sim_dir}|${sim_dir}|" tmp_input.json
   
-   # 
+   # print header of file to confirm it changed
+   echo ""
+   echo "✨🌸✨"
    head -n2 tmp_input.json 
+   echo "✨🌸✨"
 
    # Run the simulation
-   poetry run python3 src/simulator.py -l INFO -d 50 -i tmp_input.json
+   poetry run python3 src/simulator.py -l INFO -d 30 -i tmp_input.json
 
-   echo "✨🌸🌙 Finished sim set ${i}"
+   echo "🌙🌙🌙 Finished sim set ${i}"
+   echo ""
 done
 
 # Clean up temp file
 rm tmp_input.json
+
+
+
