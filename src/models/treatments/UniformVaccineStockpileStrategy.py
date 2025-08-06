6657e73b56a1481fb60cd8bcd2b58c56c0babe4b
Emily Javan, MS PhD <emjavan@gmail.com>
2025-08-06 14:37:37 -0500

    child class needs VE, updated icecream prints for clarity

--- src/models/treatments/UniformVaccineStockpileStrategy.py
+++ src/models/treatments/UniformVaccineStockpileStrategy.py
@@ -19,6 +19,8 @@ class UniformVaccineStockpileStrategy(Vaccination):
             network (Network): Network object with all nodes
         """
         self.parameters    = vaccine_model.parameters
+        # VE defined in parent class as it's needed by travel and disease model even with no vaccination, i.e. VE=0
+        self.vaccine_effectiveness = vaccine_model.vaccine_effectiveness
 
         # need to re-name, it is int days of vaccine half life (60 days)
         self.vaccine_wastage_factor = float(self.parameters.vaccine_parameters.get('vaccine_wastage_factor', 0))
@@ -28,6 +30,7 @@ class UniformVaccineStockpileStrategy(Vaccination):
         # non-negative int of when vax is effective (14 days)
         input_vax_lag               = self.parameters.vaccine_parameters.get('vaccine_eff_lag_days', 0)
         self.vaccine_eff_lag_days   = max(int(input_vax_lag), 0)
+
         # list of dictionaries but in non-stockpile strategy will be CSV file with re-name from "stockpile" to vax given
         self.vaccine_stockpile      = self.parameters.vaccine_parameters.get('vaccine_stockpile', [])
 
@@ -101,6 +104,8 @@ class UniformVaccineStockpileStrategy(Vaccination):
 
         logger.debug(f"Day {day} — Node {node.node_id} gets {node_share} vaccines; "
                     f"{self.stockpile_by_day[day]:.0f} remaining")
+        ic(f"Day {day} — Node {node.node_id} gets {node_share} vaccines; "
+                    f"{self.stockpile_by_day[day]:.0f} remaining")
 
         # Allocate vaccines released on day
         leftover_vax = self._allocate_within_node(node, node_share)
@@ -110,6 +115,7 @@ class UniformVaccineStockpileStrategy(Vaccination):
             self.stockpile_by_day.setdefault(day + 1, 0.0)
             self.stockpile_by_day[day + 1] += leftover_vax
             logger.debug(f"Day {day}: {leftover_vax} leftover vaccines rolled over to day {day + 1}.")
+            ic(f"Day {day}: {leftover_vax} leftover vaccines rolled over to day {day + 1}.")
 
     def _allocate_within_node(self, node, available_vaccines):
         # Prepare a snapshot of today's compartment state so we aren't using updated values mid-loop
@@ -166,19 +172,20 @@ class UniformVaccineStockpileStrategy(Vaccination):
             '''
             # Compute expected vaccines for this group (share * adherence)
             num_to_vaccinate = round((vaccines_left * proportion * adherence), 0)
-            ic(num_to_vaccinate)
+            ic("First calc:", num_to_vaccinate)
             # Safety: don't assign more than what's left
             num_to_vaccinate = round(min(num_to_vaccinate, vaccines_left), 0)
-            ic(num_to_vaccinate)
+            ic("Vaccines left cap:", num_to_vaccinate)
 
             # Cap at the number of susceptible people in this group (can't vaccinate more than exist)
             num_to_vaccinate = round(min(num_to_vaccinate, sus_unvax), 0)
-            ic(num_to_vaccinate)
+            ic("Sus Unvax cap:", num_to_vaccinate)
 
-            ic(VaccineGroup.U.value)
+            ic(VaccineGroup.U.value) # 0 as this is the unvax group
+            ic(VaccineGroup.V.value) # 1 as it's vaccinated group
             # Build the group descriptors
             unvax_group = Group(age=age, risk_group=risk, vaccine_group=VaccineGroup.U.value)
-            vax_group = Group(age=age, risk_group=risk, vaccine_group=VaccineGroup.V.value)
+            vax_group   = Group(age=age, risk_group=risk, vaccine_group=VaccineGroup.V.value)
 
             # Actually move the people between compartments
             self.vaccinate_number_of_people(
